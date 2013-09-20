57c7ae1450b8253929b5b926029028ffe203830c
Jeff Rynhart <jeff.rynhart@du.edu>
2013-09-20 14:22:48 -0600

    test commit for server pull

---
+++ application/controllers/email.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+ * Donor Application
+ *
+ * Email functions
+ *
+ * Author: Jeff Rynhart
+ * 
+ * University of Denver, September 2013
+ */
+class Email extends CI_Controller {
+
+    function __construct() 
+    {
+        parent::__construct();
+
+    }
+
+}
