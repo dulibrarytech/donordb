afe73755e5feb2f94d296207020328a1c1b57470
Jeff Rynhart <jeff.rynhart@du.edu>
2013-10-03 11:44:46 -0600

    added google font for app title

--- application/views/gift-view.php
+++ application/views/gift-view.php
@@ -5,8 +5,7 @@
 
 <head>
 
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css" />
+	<?php $this->load->view("partials/head-partial.php"); ?>
 
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/jquery-ui.css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/login-form.css">
---
+++ application/views/partials/head-partial.php
@@ -0,0 +1,4 @@
+<link href='http://fonts.googleapis.com/css?family=Amarante' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css" />
