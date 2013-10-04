5d5ad115d8684bac03f937c0e26e52173cb67d4a
Jeff Rynhart <jeff.rynhart@du.edu>
2013-10-04 15:46:55 -0600

    updated title font

--- application/views/partials/head-partial.php
+++ application/views/partials/head-partial.php
@@ -1,4 +1,4 @@
-<link href='http://fonts.googleapis.com/css?family=Amarante' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css" />
