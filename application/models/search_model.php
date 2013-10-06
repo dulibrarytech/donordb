853701c9f5637413feaf7f2e155cf358551bcacd
Jeff M. Rynhart <jeff.rynhart@my.denver.coloradotech.edu>
2013-10-06 11:46:06 -0600

    still got error message (prev commit) set letterFlag to -1 upon declaration

--- application/models/search_model.php
+++ application/models/search_model.php
@@ -477,6 +477,7 @@ class Search_model extends CI_Model
     public function getGiftData($giftID)
     {
         $giftInfo = array("giftDescription" => "Error: Null giftID");
+        $giftInfo['letterFlag'] = -1;
 
         if($giftID != null)
         {
